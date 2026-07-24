f5d81376edbd06504e63bc63009c67f9b13dd525
manoj173singh <manoj45singh@gmail.com>
2026-07-24 06:54:47 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.52.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "infra_rg"
+    storage_account_name = "namostg123"
+    container_name = "infracontainer"
+    key = "rg.tfstate"
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "80357092-9dcb-4069-b418-ce29822785dc"
+  # Configuration options
+}
